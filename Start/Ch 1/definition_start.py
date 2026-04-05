34a1ff18728aca3cff79a4136fa22dd96816700a
Tommy Lee <105918831+tommylee0923@users.noreply.github.com>
2026-04-05 18:36:36 +0000

    Ch1 definiteion_start

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,16 @@
 
 
 # TODO: create a basic class
+class Book:
 
+  # Initializer function
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("Brave New World")
+book2 = Book("War an Peace")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
