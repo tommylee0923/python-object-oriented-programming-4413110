3c9b2bfcd169f759cf41ed3e3fade3ee83effc5b
Tommy Lee <105918831+tommylee0923@users.noreply.github.com>
2026-04-06 02:10:37 +0000

    CH2 multiple_start.py

--- Start/Ch 2/multiple_start.py
+++ Start/Ch 2/multiple_start.py
@@ -6,17 +6,25 @@ class A:
     def __init__(self):
         super().__init__()
         self.prop1 = "prop1"
+        self.name = "Class A"
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.prop2 = "prop2"
+        self.name = "Class B"
 
 
 class C(A, B):
     def __init__(self):
         super().__init__()
 
+    def showprops(self):
+        print(self.prop1)
+        print(self.prop2)
+        print(self.name) # Python method resolution order
 
 c = C()
+print(C.__mro__)
+c.showprops()
